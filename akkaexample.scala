12362ff2f20f73fe441193ce1bcb8d2f32a5aac5
Anton Shliakthtin <anton.shliakhtin@ibm.com>
2021-12-22 01:28:41 +0200

    - init commit

---
+++ akkaexample.scala
@@ -0,0 +1,16 @@
+import akka.actor.{Actor, ActorSystem, Props}
+
+class HelloActor extends Actor {
+  def receive: Receive = {
+    case "hello" => println("hello back at you")
+    case _       => println("huh?")
+  }
+}
+
+object Main extends App {
+  val system = ActorSystem("HelloSystem")
+  // default Actor constructor
+  val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
+  helloActor ! "hello"
+  helloActor ! "buenos dias"
+}
